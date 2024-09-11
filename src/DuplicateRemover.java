26570b5cb60c165e84ea4ae2f52cfdc273e38267
Welk-dy <Fernaldysaputra@gmail.com>
2024-09-11 10:13:43 +0700

    first commit

---
+++ src/DuplicateRemover.java
@@ -0,0 +1,25 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class DuplicateRemover {
+
+    // Function to remove duplicates from an array
+    public int[] removeDuplicates(int[] nums) {
+        // Using LinkedHashSet to maintain insertion order and remove duplicates
+        Set<Integer> uniqueElements = new LinkedHashSet<>();
+
+        // Add each element of the array to the LinkedHashSet
+        for (int num : nums) {
+            uniqueElements.add(num);
+        }
+
+        // Convert the LinkedHashSet back to an array
+        int[] result = new int[uniqueElements.size()];
+        int index = 0;
+        for (int num : uniqueElements) {
+            result[index++] = num;
+        }
+
+        return result;
+    }
+}
